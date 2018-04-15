f753af0d13d3f12d5633c2e902727b9f9710d5da
Chris Quinn <cmquinn976@gmail.com>
2018-04-15 12:53:39 -0600

    Min algorithm working

---
+++ MIN.java
@@ -0,0 +1,131 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MIN {
+	char[] arr;
+	private int rows = Table.pubRows;
+	private int length = Table.pubLength;
+	char[][] tableSlots = new char[rows][length];
+	List<Character> active = new ArrayList<Character>(); //the current pages that are active [0] would be the first in
+	Map<String, String> map = new HashMap<String, String>(); //keeps track of what row the active pages are in
+	String pattern = Table.pattern;
+	List<Character> patList = new ArrayList<Character>();
+	
+	public MIN(char[] arr){
+		this.arr = arr;
+	}
+	public void MainMin(){
+		
+		for(char elem : pattern.toCharArray()){
+			patList.add(elem);
+		}
+		
+		int x = 0;
+		for(int i = 0; i<arr.length; i++,x++){
+						
+			if(x > rows - 1){x = 0;} //x is keeping track of the rows
+			
+			
+			if(!map.containsKey(Character.toString(arr[i]))) //if this is a new page
+			{
+				newPage(arr, i, x);	
+				patList.remove(0);
+				continue;
+			}
+			else if(map.containsKey(Character.toString(arr[i]))){ // not a new page ( page hit)
+				String newtemp = map.get(Character.toString(arr[i]));
+				tableSlots[Integer.valueOf(newtemp)][i] = "+".charAt(0);
+				x--;
+				patList.remove(0);
+			}
+			
+		}
+		
+		System.out.println( "Ref Str: " + pattern);
+		int n = 1;
+		for(char[] row : tableSlots) {
+			System.out.print("MIN " + n + ": ");
+			System.out.print("\t");
+			n++;
+            printRow(row);
+        }
+	}
+	
+	//so i can search hash map by value
+	public static Object getKeyFromValue(Map<String, String> hm, Object value) {
+	    for (Object o : hm.keySet()) {
+	      if (hm.get(o).equals(value)) {
+	        return o;
+	      }
+	    }
+	    return null;
+	  }
+	
+	public void newPage(char[] arr, int i, int x){
+		//if there is no row which has not been filled yet
+		if(active.size() > (rows - 1)){ 
+			
+			char toRemove = minRemove();
+			String temp = (String) getKeyFromValue(map,map.get(Character.toString((char) active.get(active.indexOf(toRemove)))));
+			map.put(Character.toString(arr[i]) , map.get(temp));
+			map.remove(temp);
+			active.remove(active.indexOf(toRemove));
+			active.add(arr[i]);
+			String newtemp = map.get(Character.toString(arr[i]));
+			tableSlots[Integer.valueOf(newtemp)][i] = arr[i];
+		//if there is still an empty row	
+		}else{
+			tableSlots[x][i] = arr[i];
+			active.add(arr[i]); 
+			map.put(Character.toString(arr[i]), Integer.toString(x));
+			
+		}	
+	}
+	
+	 public static void printRow(char[] row) { //format printing
+	        for (char i : row) {
+	        	if(i == 0){
+	        		System.out.print("*");
+	        		System.out.print("\t");
+	        	}else{
+	        		System.out.print(i);
+		            System.out.print("\t");
+	        	}
+	            
+	        }
+	        System.out.println();
+	    }
+	 
+	 public char minRemove(){
+		 char toRemove = 0;
+			List<Character> minArray = new ArrayList<Character>();
+			for(char elem : patList){
+				if(minArray.size() < rows){
+					if(active.contains(elem) & !(minArray.contains(elem))){
+						minArray.add(elem);
+					}
+				}
+			}
+			if(minArray.size() == 0){
+				toRemove = active.get(0);
+			}
+			if(minArray.size() > 0){
+				toRemove = minArray.get((minArray.size() - 1));
+			}
+			if (minArray.size() < active.size()){
+				for(char add : active){
+					if(!(minArray.contains(add))){
+						minArray.add(add);
+					}
+				}
+				toRemove = minArray.get((minArray.size() - 1));
+			}
+			return toRemove;
+	 }
+	
+	
+}
+
--- Main.java
+++ Main.java
@@ -6,10 +6,12 @@ public class Main {
 	 static char[] pattern = Table.CreatePattern();
 
 	public static void main(String[] args) {
-		LRU lru = new LRU(pattern);
-		lru.MainLru();
-		FIFO fifo = new FIFO(pattern);
-		fifo.MainFifo();
+//		LRU lru = new LRU(pattern);
+//		lru.MainLru();
+//		FIFO fifo = new FIFO(pattern);
+//		fifo.MainFifo();
+		MIN min = new MIN(pattern);
+		min.MainMin();
 
 	}
 	
