d96ae2ce089039bad04a59152e72a425833b1433
Chris Quinn <cmquinn976@gmail.com>
2018-04-15 14:49:39 -0600

    All algorithms implemented

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-import java.util.Random;
-import java.util.Scanner;
+
 
 
 public class Main {
 	 static char[] pattern = Table.CreatePattern();
 
 	public static void main(String[] args) {
-//		LRU lru = new LRU(pattern);
-//		lru.MainLru();
-//		FIFO fifo = new FIFO(pattern);
-//		fifo.MainFifo();
+		FIFO fifo = new FIFO(pattern);
+		fifo.MainFifo();
+		LRU lru = new LRU(pattern);
+		lru.MainLru();
 		MIN min = new MIN(pattern);
 		min.MainMin();
+		RAND rand = new RAND(pattern);
+		rand.MainRand();
 
 	}
 	
